af707b238b16813aa2b508dd45c18ab4f5b054aa
Kaya Jackson <amanij7@gmail.com>
2018-07-10 12:18:54 -0400

    components album 1

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component {
+  constructor(props){
+    super(props);
+    const album = albumData.find( album=> {
+      return album.slug === this.props.match.params.slug
+    });
+    this.state = {
+      album: album
+    }
+  }
   render() {
     return (
       <section className="album">
-        {this.props.match.params.slug} Album will go here
+      <section id="album-info">
+        <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+        <div className="album-details">
+            <h1 id="album-title"></h1>
+            <h2 className="artist"></h2>
+            <div id="release-info"></div>
+          </div>
+        </section>
+      <table is="song-list">
+        <colgroup>
+          <col id="song-number-column" />
+          <col id="song-title-column" />
+          <col id="song-duration-column" />
+        </colgroup>
+        <tbody>
+        {this.state.albums.songs.map((songs, index) =>
+        <tr key={index}>
+          <td>{this.title}</td>
+          <td>{this.duration}</td>
+        </tr>
+      )
+    }
+        </tbody>
+      </table>
       </section>
     );
   }
