0239c4f766e2244c778b260b1713455ada358cee
Kaya Jackson <amanij7@gmail.com>
2018-07-18 12:28:11 -0400

    styling 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album'
+import './index.css';
 
 class App extends Component {
   render() {
@@ -14,7 +15,7 @@ class App extends Component {
          <Link to='/'>Landing</Link>
          <Link to='/library'>Library</Link>
         </nav>
-          <h1>Bloc Jams</h1>
+          <h1 id="title"> Bloc Jams</h1>
         </header>
         <p className="App-intro">
         </p>
